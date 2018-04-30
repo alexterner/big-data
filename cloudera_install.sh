bdd3a427e8a7faa3a707d698dcfd8d5ca89e9a1f
Alex Terner <AlexTerner@gmail.com>
2018-04-30 16:23:28 +0300

    Install cloudera via docker

---
+++ cloudera_install.sh
@@ -0,0 +1,11 @@
+ # install docker to serevr
+sudo apt-get -y install docker.io
+ 
+ # download docker image from repository
+sudo docker pull cloudera/quickstart:latest
+  
+ # print list of docker images - you need to take hash id
+sudo docker images
+ 
+ # create docker container from cloudera image and run it
+sudo docker run --hostname=quickstart.cloudera --privileged=true -t -i -p 8888:8888 -p 21050:21050 INSERT_HASH_CODE_HERE /usr/bin/docker-quickstart

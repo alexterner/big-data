ea29e1b28856aee6313d354fc3d6e104f6e8e469
Alex Terner <AlexTerner@gmail.com>
2018-04-30 20:29:10 +0300

    Adding run cloudera manager and cloudera manager port 7180

--- cloudera_install.sh
+++ cloudera_install.sh
@@ -8,4 +8,7 @@ sudo docker pull cloudera/quickstart:latest
 sudo docker images
  
  # create docker container from cloudera image and run it
-sudo docker run --hostname=quickstart.cloudera --privileged=true -t -i -p 8888:8888 -p 21050:21050 INSERT_HASH_CODE_HERE /usr/bin/docker-quickstart
+sudo docker run --hostname=quickstart.cloudera --privileged=true -t -i -p 8888:8888 -p 21050:21050 -p 7180:7180 INSERT_HASH_CODE_HERE /usr/bin/docker-quickstart
+
+# Start cloudera manager
+sudo service cloudera-scm-server start
